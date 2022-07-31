f9a1d22e7f3a779a6c33659617f187ea729e1e6e
adityabhartib <adityabharti.ab@gmail.com>
2022-08-01 00:08:32 +0530

    0-1-Tringle Added

--- src/com/Bridgelabz/Javaforloop.java
+++ src/com/Bridgelabz/Javaforloop.java
@@ -3,19 +3,18 @@ public class Javaforloop {
 
     public static void main(String[] args) {
         int n = 5;
-        int number = 1;
 
         //outerloop
-       for(int i=1; i<=n; i++) {
-           //inner loop
-          for (int j=1; j<=i; j++){
-              System.out.print(number+" ");
-              number++;//number  = number + 1
-
-          }
-           System.out.println();
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) { // even
+                    System.out.print("1 ");
+                } else { //odd
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+            }
        }
     }
-
-
-}
