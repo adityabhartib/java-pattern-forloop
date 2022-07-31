e1653c1a6e2081a503e8474bcd6617e2100b5073
adityabhartib <adityabharti.ab@gmail.com>
2022-07-31 23:39:55 +0530

    half piramid with number

--- src/com/Bridgelabz/Javaforloop.java
+++ src/com/Bridgelabz/Javaforloop.java
@@ -2,19 +2,13 @@ package com.Bridgelabz;
 public class Javaforloop {
 
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
 
         //outerloop
        for(int i=1; i<=n; i++) {
-           //inner lop -> space print
-           for(int j=1; j<=n-i; j++) {
-               System.out.print(" ");
-
-           }
-           // inner loop -> star print
-           for (int j=1; j<=i; j++) {
-               System.out.print("*");
-           }
+          for (int j=1; j<=i; j++){
+              System.out.print(j+" ");
+          }
            System.out.println();
        }
     }
