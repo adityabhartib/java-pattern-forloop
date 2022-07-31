9a6ce970395944e6da0a90c8c46095b9add7cec9
adityabhartib <adityabharti.ab@gmail.com>
2022-07-31 23:56:06 +0530

    floiyd tringe

--- src/com/Bridgelabz/Javaforloop.java
+++ src/com/Bridgelabz/Javaforloop.java
@@ -3,12 +3,15 @@ public class Javaforloop {
 
     public static void main(String[] args) {
         int n = 5;
+        int number = 1;
 
         //outerloop
        for(int i=1; i<=n; i++) {
            //inner loop
-          for (int j=1; j<=n-i+1; j++){
-              System.out.print(j);
+          for (int j=1; j<=i; j++){
+              System.out.print(number+" ");
+              number++;//number  = number + 1
+
           }
            System.out.println();
        }
