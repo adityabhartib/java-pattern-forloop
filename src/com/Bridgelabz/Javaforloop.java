76ba38b439a5de59f23b30ad89fd19a962844eda
adityabhartib <adityabharti.ab@gmail.com>
2022-07-31 23:05:16 +0530

    inverted half piuramid added

--- src/com/Bridgelabz/Javaforloop.java
+++ src/com/Bridgelabz/Javaforloop.java
@@ -5,12 +5,12 @@ public class Javaforloop {
         int n = 4;
 
         //outerloop
-        for(int i=1; i<=n; i++) {
+        for (int i=n; i>=1; i--) {
             //inner loop
-            for(int j=1; j<=i; j++) {
+            for (int j=1; j<=i; j++) {
                 //cell-> (i,j)
-                    System.out.print("*");
-                }
+                System.out.print("*");
+            }
                 System.out.println();
             }
         }
